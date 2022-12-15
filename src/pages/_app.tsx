0e81cb5cf957e6d16d672582adddff4182ca8463
Tekena Solomon <tekena157@gmail.com>
2022-12-15 19:06:36 +0100

    Add OG image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 import Affix from "../components/Affix";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          property="og:image"
+          content="https://sea-lion-app-ncths.ondigitalocean.app/og_image.png"
+        />
+      </Head>
       <Component {...pageProps} />
       <Affix />
     </>
